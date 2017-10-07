2fba720c73dc4bb7e8466271d989b2aa315392db
logonrm <logonrm@fiap.com.br>
2017-10-07 14:43:22 -0300

    Initial commit

---
+++ app/src/main/java/com/example/logonrm/shiftcarros/adapter/TabAdapter.java
@@ -0,0 +1,25 @@
+package com.example.logonrm.shiftcarros.adapter;
+
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentStatePagerAdapter;
+
+/**
+ * Created by logonrm on 07/10/2017.
+ */
+
+public class TabAdapter extends FragmentStatePagerAdapter{
+    public TabAdapter(FragmentManager fm) {
+        super(fm);
+    }
+
+    @Override
+    public Fragment getItem(int position) {
+        return null;
+    }
+
+    @Override
+    public int getCount() {
+        return 0;
+    }
+}
